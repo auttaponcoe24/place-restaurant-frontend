fc9579760c9de9d4b8002e55a68cf2544dd2a964
auttpon <auttaponcoe24@gmail.com>
2023-12-02 00:04:32 +0700

    restaurant detail:done

--- src/pages/DetailRestaurant.jsx
+++ src/pages/DetailRestaurant.jsx
@@ -1,5 +1,84 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useRestaurant from "../hooks/use-restaurant";
+import { useParams } from "react-router-dom";
 
 export default function DetailRestaurant() {
-	return <div>DetailRestaurant</div>;
+	const { fetchRestaurant, restaurant } = useRestaurant();
+	const { resId } = useParams();
+
+	useEffect(() => {
+		fetchRestaurant();
+	}, []);
+	// console.log(restaurant);
+
+	const restaurantById = restaurant.find((item) => item.id === +resId);
+	console.log("restaurantById =>", restaurantById);
+
+	return (
+		<div className="bg-[#C4D3E9] m-6 p-4 rounded-lg">
+			<div className="grid grid-cols-12 px-4 py-2 gap-8">
+				{/* left */}
+				<div className="col-span-8 ">
+					<div className="h-[400px] overflow-hidden">
+						<img
+							src={restaurantById?.profile_image_url}
+							alt=""
+							className="h-full w-full object-cover rounded-t-lg"
+						/>
+					</div>
+					<div className="flex flex-col gap-4 bg-white rounded-b-lg p-4">
+						<div className="flex items-center justify-between">
+							<h1 className="uppercase text-lg font-bold  ">
+								{restaurantById?.name}
+							</h1>
+							<div className="flex items-center gap-2">
+								<div className="w-3 h-3 bg-primary rounded-full"></div>
+								<div className="text-md">{restaurantById?.rating}</div>
+							</div>
+						</div>
+						<div className="grid grid-cols-12">
+							<div className="col-span-3 font-semibold">Address:</div>
+							<div className="col-span-9">{restaurantById?.address}</div>
+						</div>
+						<div className="grid grid-cols-12">
+							<div className="col-span-3 font-semibold">Address:</div>
+							<div className="col-span-9">
+								{restaurantById?.operation_time.map((item, index) => (
+									<div key={index}>
+										{item.day}: {item.time_open} AM - {item.time_close} PM
+									</div>
+								))}
+							</div>
+						</div>
+					</div>
+				</div>
+
+				{/* right */}
+				<div className="col-span-4 p-6 flex flex-col bg-white rounded-lg">
+					<h1 className="text-xl mb-4">Images</h1>
+					<div className="max-w-[479px] h-[318px] overflow-hidden">
+						<img
+							src={restaurantById?.images[0]}
+							alt="images0"
+							className="w-full h-full rounded-t-xl object-cover"
+						/>
+					</div>
+					<div className="max-w-[479px] h-[318px] overflow-hidden">
+						<img
+							src={restaurantById?.images[1]}
+							alt="images1"
+							className="w-full h-full object-cover"
+						/>
+					</div>
+					<div className="max-w-[479px] h-[318px] overflow-hidden">
+						<img
+							src={restaurantById?.images[2]}
+							alt="images2"
+							className="w-full h-full rounded-b-xl object-cover"
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import useRestaurant from "../hooks/use-restaurant";
 import Button from "../components/Button";
 import RestaurantList from "../features/restaurant/RestaurantList";
 import { Input } from "@material-tailwind/react";
+import { IoMdSearch } from "react-icons/io";
 
 export default function Home() {
 	const { fetchRestaurant, restaurant } = useRestaurant();
@@ -22,7 +23,7 @@ export default function Home() {
 				<div className="flex items-center gap-4">
 					<div className="relative h-10 w-72 min-w-[200px]">
 						<select
-							className="border border-gray-500 w-full px-4 py-2 rounded-lg outline-none text-gray-500 text-sm"
+							className="border border-gray-400 w-full p-2 rounded-lg outline-none text-[#605C5C] text-sm"
 							label="Restaurant"
 							value={selected}
 							onChange={(e) => setSelected(e.target.value)}
@@ -33,10 +34,14 @@ export default function Home() {
 						</select>
 					</div>
 					<div> | </div>
-					<div className="w-72">
+					<div className="w-72 relative">
 						<Input
 							label="Search name..."
 							onChange={(e) => setSearch(e.target.value)}
+						/>
+						<IoMdSearch
+							className="z-10 absolute top-2 right-2 text-[#605C5C]"
+							size={25}
 						/>
 					</div>
 				</div>
